4b5740e1c657063265b0aaee7ee16e5301e332d1
unknown <luckyriteshmalviya@gmail.com>
2025-04-15 00:53:45 +0530

    handled errors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const serverless = require('serverless-http');
-const cartRoutes = require("./routes/cartRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const productRoutes = require("./routes/productRoutes");
-const reportRoutes = require("./routes/reportRoutes");
-const authRoutes = require("./routes/authRoutes");
-const auth = require("./middleware/auth");
+// const cartRoutes = require("./routes/cartRoutes");
+// const orderRoutes = require("./routes/orderRoutes");
+// const productRoutes = require("./routes/productRoutes");
+// const reportRoutes = require("./routes/reportRoutes");
+// const authRoutes = require("./routes/authRoutes");
+// const auth = require("./middleware/auth");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,11 +17,11 @@ app.use(express.json());
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/cart", auth, cartRoutes);
-app.use("/api/orders", auth, orderRoutes);
-app.use("/api/products", auth, productRoutes);
-app.use("/api/reports", auth, reportRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/cart", auth, cartRoutes);
+// app.use("/api/orders", auth, orderRoutes);
+// app.use("/api/products", auth, productRoutes);
+// app.use("/api/reports", auth, reportRoutes);
 
 // Basic route
 app.get("/", (req, res) => {
